3ea052ce1920498c9176e72e2dbf0beee30e2971
xiejunze@sogou-inc.com <xiejunze@sogou-inc.com>
2021-12-19 18:23:02 +0800

    删除无用代码

--- main.go
+++ main.go
@@ -11,16 +11,6 @@ func main() {
 	getHomeWorkHanderMng().getHomeworkHandler("homeworkSecond").homeworkDoSomething(context.Background())
 }
 
-func funcName(ch chan int) {
-	select {
-	case i := <-ch:
-		println(i)
-
-	default:
-		println("default")
-	}
-}
-
 func init() {
 	orm.RegisterDriver("mysql", orm.DRMySQL)
 	orm.RegisterDataBase("default", "mysql", "root:qishiwang5@tcp(127.0.0.1:3306)/slo_test?charset=utf8&parseTime=true")
